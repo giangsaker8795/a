427b80f5c78b686d0d4d157d95e39b133ce1e677
Hoàng Văn Giảng <31692191+giangsaker8795@users.noreply.github.com>
2017-10-02 00:22:05 +0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 if (!defined('ACCESS')) die('Not access');
 else $configs = array(
     'username' => 'admin',
-    'password' => '1f32aa4c9a1d2ea010adcf2348166a04',
+    'password' => '072eeb0162a24c3ec28bceeee7b234e1',
     'page_list' => '0',
     'page_file_edit' => '0',
     'page_file_edit_line' => '0',
